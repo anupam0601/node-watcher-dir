54a9a8b622e44fabab14f60c559d72c3ed104276
adebnath1989@gmail.com <adebnath1989@gmail.com>
2016-08-01 00:21:33 +0530

    added chokidar and slack integration for file change

---
+++ watch-dir-slack.js
@@ -0,0 +1,44 @@
+// Module to watch files for changes and push it to slack:::
+
+var chokidar = require('chokidar'); // Chokidar module
+var Slack = require('slack-node'); // Slack module
+
+// Date with formatted string --- 2016-07-31 17:49:42
+var date = new Date().toISOString().
+  replace(/T/, ' ').      // replace T with a space
+  replace(/\..+/, '')     // delete the dot and everything after
+ 
+
+// Web hook Uri for slack
+webhookUri = "__uri__";
+
+// Slack params
+slack = new Slack();
+slack.setWebhook(webhookUri);
+
+
+// Initialize watcher. 
+var watcher = chokidar.watch('/Users/anupamdebnath/nodeScripts/node-watcher-slack/error.log', {
+  ignored: /[\/\\]\./,
+  persistent: true
+});
+
+//Something to use when events are received. 
+var log = console.log.bind(console);
+
+// The following watches for events and if error.log chnages it shoots a message to slack
+
+watcher
+
+  // When file changes slack message is initiated
+  .on('change', path => slack.webhook({
+	  channel: "code-check",
+	  username: "webhookbot",
+	  text: "There are changes in error.log"
+	}, function(err, response) {
+	  console.log(response);
+	})
+  )
+  .on('unlink', path => log(date,`File ${path} has been removed`));
+
+

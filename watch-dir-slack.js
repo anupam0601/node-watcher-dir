ed8c2a40a5387688368e4a73fa95228c3bf9250a
Anupam Debnath <adebnath1989@gmail.com>
2016-11-24 00:32:11 +0530

    added code for printing last 5 lines of log file

--- watch-dir-slack.js
+++ watch-dir-slack.js
@@ -2,6 +2,7 @@
 
 var chokidar = require('chokidar'); // Chokidar module
 var Slack = require('slack-node'); // Slack module
+const exec = require('child_process').exec; //For shell commands
 
 // Date with formatted string --- 2016-07-31 17:49:42
 var date = new Date().toISOString().
@@ -18,7 +19,7 @@ slack.setWebhook(webhookUri);
 
 
 // Initialize watcher. 
-var watcher = chokidar.watch('/<path>/error.log', {
+var watcher = chokidar.watch('/Users/anupamdebnath/nodeScripts/node-watcher-slack/error.log', {
   ignored: /[\/\\]\./,
   persistent: true
 });
@@ -30,14 +31,21 @@ var log = console.log.bind(console);
 
 watcher
 
-  // When file changes slack message is initiated
-  .on('change', path => slack.webhook({
-	  channel: "#general",
-	  username: "webhookbot",
-	  text: "There are changes in error.log"
-	}, function(err, response) {
-	  console.log(response);
-	})
+  /*
+    1. When there are changes:
+      a. tails the log for last 5 lines
+      b. Initiates a slack message on the web hook channel
+  */
+
+  .on('change', path => exec("tail -n 5 error.log", function (error, stdout, stderr) {
+      slack.webhook({
+        channel: "code-check",
+        username: "webhookbot",
+        text:"Log has changes: " + stdout
+      }, function(err, response) {
+        console.log(response);
+      })
+    })
   )
   .on('unlink', path => log(date,`File ${path} has been removed`));
 
